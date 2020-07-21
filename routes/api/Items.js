3ea5d48ea1f71757701f571c6edfa896f72b97e3
sowmyamaddala6 <sowmyamaddala6@gmail.com>
2020-07-21 20:18:52 +0530

    first commit

---
+++ routes/api/Items.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+
+//Item model
+const Item = require('../../models/Item');
+
+// @route GET api/items
+//@desc  Get all items
+//@access public
+router.get('/', (req, res) => {
+    Item.find()
+        .sort({ date: -1 })
+        .then(items => res.json(items))
+});
+
+//@route  POST api/items
+//@desc   Create an Item
+//@access private
+router.post('/', auth, (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    newItem.save().then(item => res.json(item));
+});
+
+//@route  POST api/items/:id
+//@desc   Delete an Item
+//@access private
+router.delete('/:id', auth, (req, res) => {
+    Item.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({success: true})))
+        .catch(err => res.status(404).json({success: false}));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const config = require('config');
+
+const app = express();
+
+//Bodyparser Middleware
+app.use(express.json());
+
+//DB config
+const db = config.get('mongoURI');
+
+//Connect a mongo
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }) // Adding new mongo url parser
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+//Use routes
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+// Serve static assests if in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
